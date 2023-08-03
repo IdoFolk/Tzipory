9a0a772cbe1c5980d23ec6a3fa0d485441278a2a
Alon <Alonsouter@gmail.com>
2023-08-03 12:49:30 +0300

    Made bettert

--- Assets/Scripts/Systems/VisualSystem/PopUpTexter.cs
+++ Assets/Scripts/Systems/VisualSystem/PopUpTexter.cs
@@ -11,8 +11,8 @@ public class PopUpTexter
     [SerializeField] GameObject _textBoxPrefab;
     [SerializeField] Transform _textSpawnPoint;
 
-    [SerializeField] float _sineMagnitude;
-    [SerializeField] float _sineStep;
+    [SerializeField] float _lineMagnitude;
+    [SerializeField] float _lineStep;
     float _currentSinePlace = 0f;
     
      public void SpawnPopUp(PopUpText_Config config)
@@ -22,24 +22,24 @@ public class PopUpTexter
         switch (_repeatPattern)
         {
             case TextSpawnRepeatPatterns.PingPong:
-                value.x = Mathf.Sin(_currentSinePlace) * _sineMagnitude;
-                _currentSinePlace += _sineStep;
+                value.x = Mathf.Sin(_currentSinePlace) * _lineMagnitude;
+                _currentSinePlace += _lineStep;
 
                 break;
             case TextSpawnRepeatPatterns.LoopRight:
-                if (_currentSinePlace >= _sineMagnitude)
-                    _currentSinePlace = _sineMagnitude * -1f;
+                if (_currentSinePlace >= _lineMagnitude)
+                    _currentSinePlace = _lineMagnitude * -1f;
 
                 value.x = _currentSinePlace;
-                _currentSinePlace += _sineStep;
+                _currentSinePlace += _lineStep;
 
                 break;
             case TextSpawnRepeatPatterns.LoopLeft:
-                if (_currentSinePlace <= _sineMagnitude *-1f)
-                    _currentSinePlace = _sineMagnitude;
+                if (_currentSinePlace <= _lineMagnitude *-1f)
+                    _currentSinePlace = _lineMagnitude;
 
                 value.x = _currentSinePlace;
-                _currentSinePlace -= _sineStep;
+                _currentSinePlace -= _lineStep;
                 break;
             default:
                 break;

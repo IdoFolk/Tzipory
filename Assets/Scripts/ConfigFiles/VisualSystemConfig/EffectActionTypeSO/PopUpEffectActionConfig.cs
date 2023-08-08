d22c79676faec2eec4c1233a760c8788ceea7395
Alon <Alonsouter@gmail.com>
2023-08-08 18:46:51 +0300

    Fix for Size call

--- Assets/Scripts/ConfigFiles/VisualSystemConfig/EffectActionTypeSO/PopUpEffectActionConfig.cs
+++ Assets/Scripts/ConfigFiles/VisualSystemConfig/EffectActionTypeSO/PopUpEffectActionConfig.cs
@@ -13,11 +13,21 @@ namespace Tzipory.VisualSystem.EffectSequence.EffectActionTypeSO
 
         public override EffectActionType ActionType => EffectActionType.PopUp;
 
+        /// <summary>
+        /// For size fix
+        /// </summary>
+        [SerializeField] LevelVisualDataSO _levelVisualDataSO;
+
 #if UNITY_EDITOR
         [ContextMenu("Set Size Relative to Damage")]
         public void CallConfigSizeFix()
         {
-            PopUpText_Config.SetSizeRelativeToDamage();
+            if(PopUpText_Config.damage <= 0)
+            {
+                Debug.LogError("damage amount is 0 or less - not fixing size");
+                return;
+            }
+            PopUpText_Config.size = _levelVisualDataSO.GetRelativeFontSizeForDamage(PopUpText_Config.damage);
         }
 #endif 
     }
--- Assets/Scripts/Testing/LevelVisualDataSO.cs
+++ Assets/Scripts/Testing/LevelVisualDataSO.cs
@@ -6,4 +6,6 @@ using UnityEngine;
 public class LevelVisualDataSO : ScriptableObject
 {
     public Level_VisualData level_VisualData;
+    public float GetRelativeFontSizeForDamage(float damage) => Mathf.Clamp((level_VisualData.FontSizeRange.y - level_VisualData.FontSizeRange.x) * (damage - level_VisualData.DamageRange.x) / (level_VisualData.DamageRange.y - level_VisualData.DamageRange.x) + level_VisualData.FontSizeRange.x, level_VisualData.FontSizeRange.x, level_VisualData.FontSizeRange.y);
+
 }

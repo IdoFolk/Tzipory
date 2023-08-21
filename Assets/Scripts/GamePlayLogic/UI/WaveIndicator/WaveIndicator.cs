e5350438eeed5a93de4664f644d5de7425a40ff9
Jonnix12 <jonharel48@gmail.com>
2023-08-21 18:47:56 +0300

    Add wave Indicator pool and add base game object factory

--- Assets/Scripts/GamePlayLogic/UI/WaveIndicator/WaveIndicator.cs
+++ Assets/Scripts/GamePlayLogic/UI/WaveIndicator/WaveIndicator.cs
@@ -1,22 +1,34 @@
+using System;
+using Tzipory.Systems.PoolSystem;
 using Tzipory.Tools.Interface;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace GamePlayLogic.UI.WaveIndicator
 {
-    public class WaveIndicator : MonoBehaviour , IInitialization
+    public class WaveIndicator : MonoBehaviour , IInitialization , IPoolable<WaveIndicator>
     {
+        public event Action<WaveIndicator> OnDispose;
+        
         [SerializeField] private RectTransform _rotateIndicator;
         [SerializeField] private Image _timerFill;
-
-
-        public bool IsInitialization { get; private set; }
         
+        public bool IsInitialization { get; private set; }
         
         public void Init()
         {
             _timerFill.fillAmount = 1;
             IsInitialization  = true;
+        }
+
+        public void Dispose()
+        {
+            OnDispose?.Invoke(this);
+        }
+
+        public void Free()
+        {
+            Destroy(gameObject);
         }
     }
 }

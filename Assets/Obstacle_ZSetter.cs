2f7d7a1c8a24a24b84751aa80b6fb69ce49e9cdf
Alon <Alonsouter@gmail.com>
2023-08-21 19:04:13 +0300

    minor changes

--- Assets/Obstacle_ZSetter.cs
+++ Assets/Obstacle_ZSetter.cs
@@ -4,15 +4,9 @@ using Tzipory.EntitySystem.Entitys;
 using UnityEngine;
 
 //Not sure if temp -> more like "experimental"?
-public class SilhouetterSetter : MonoBehaviour
+public class Obstacle_ZSetter : MonoBehaviour
 {
     BaseUnitEntity _unit;
-    //[SerializeField] PolygonCollider2D _polyCol;
-
-    private void Start()
-    {
-        //gameObject.AddComponent<PolygonCollider2D>(); //this happens so the polygon sets itself automatically to whatever sprite may be set in Awake()/Init()
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {

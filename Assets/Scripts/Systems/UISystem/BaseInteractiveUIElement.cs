c9ea085121ffa13e8f7df55c886d914ee5e46e68
Jonnix12 <jonharel48@gmail.com>
2023-10-23 14:02:21 +0300

    Update to ui system

--- Assets/Scripts/Systems/UISystem/BaseInteractiveUIElement.cs
+++ Assets/Scripts/Systems/UISystem/BaseInteractiveUIElement.cs
@@ -120,14 +120,4 @@ namespace Tzipory.Systems.UISystem
         {
         }
     }
-
-    public interface IUIElement
-    {
-        public string ElementName { get; }
-        public Action OnShow { get; }
-        public Action OnHide { get; }
-        
-        void Show();
-        void Hide();
-    }
 }
---
+++ Assets/Scripts/Systems/UISystem/IUIElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tzipory.Systems.UISystem
+{
+    public interface IUIElement
+    {
+        public string ElementName { get; }
+        public Action OnShow { get; }
+        public Action OnHide { get; }
+        
+        void Show();
+        void Hide();
+    }
+}

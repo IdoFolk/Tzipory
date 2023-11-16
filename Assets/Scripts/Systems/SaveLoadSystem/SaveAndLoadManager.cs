8d7d16fcf76d7edad3a92016cdd1e893a23ace48
Jonnix12 <jonharel48@gmail.com>
2023-11-16 13:14:45 +0200

    Finale logger

--- Assets/Scripts/Systems/SaveLoadSystem/SaveAndLoadManager.cs
+++ Assets/Scripts/Systems/SaveLoadSystem/SaveAndLoadManager.cs
@@ -5,6 +5,8 @@ namespace Tzipory.Systems.SaveLoadSystem
 {
     public class SaveAndLoadManager
     {
+        public static string SAVE_SYSTEM_LOG_GROUP = "SaveSystem";
+        
         public bool GetSaveData<T>(out T output) where T : class, ISerializeData, new()
         {
             //need to add save and load logic

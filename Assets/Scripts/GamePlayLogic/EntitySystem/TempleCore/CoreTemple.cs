5664236c86ef4f4239fda0bad49cb762f75ff102
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2023-12-17 11:50:57 +0200

    Revert "Merge branch 'Animations-Ido' into Ido-Refactor-Fix"
    This reverts commit 9b6767bc56b1346852c400b26ea5581c1f63c276, reversing
    changes made to b7ad8528c66b2ac1b7b2ab40d253486774ad544d.

--- Assets/Scripts/GamePlayLogic/EntitySystem/TempleCore/CoreTemple.cs
+++ Assets/Scripts/GamePlayLogic/EntitySystem/TempleCore/CoreTemple.cs
@@ -1,78 +1,68 @@
 using System;
 using System.Collections.Generic;
+using GameplayLogic.UI.HPBar;
 using PathCreation;
-using Sirenix.OdinInspector;
-using Tzipory.GameplayLogic.EntitySystem.Enemies;
+using Tzipory.ConfigFiles.EntitySystem.ComponentConfig;
+using Tzipory.GamePlayLogic.EntitySystem;
 using Tzipory.GameplayLogic.Managers.MainGameManagers;
 using Tzipory.GameplayLogic.UI.Indicator;
 using Tzipory.Systems.Entity;
 using Tzipory.Systems.Entity.EntityComponents;
+using Tzipory.Systems.FactorySystem;
 using Tzipory.Systems.StatusSystem;
 using Tzipory.Tools.Interface;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class CoreTemple : BaseGameEntity, IEntityTargetAbleComponent
+public class CoreTemple : BaseGameEntity, ITargetAbleEntity , IInitialization
 {
     [SerializeField] private PathCreator _patrolPath;
     [SerializeField] private UIIndicatorConfig _uiIndicatorConfig;
-    [SerializeField] private SpriteRenderer _defaultCoreSpriteRenderer;
-    [SerializeField] private GameObject _brokenCoreSpriteRenderer;
-    [SerializeField] private ParticleSystem _glowParticleSystem;
-    [SerializeField] private Animator _coreAnimator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private TEMP_HP_Bar _hpBar;
     
-    [SerializeField] float _hp;
-    [SerializeField] float _deathAnimationTime;
-    [SerializeField] int _animationCameraZoom;
-
-    [SerializeField,ReadOnly] private Stat _hpStat;
+    [SerializeField] private float _hp;
     
-    private List<Enemy> _enemies = new List<Enemy>();
+    private readonly List<UnitEntity> _enemies = new();
     
     private IObjectDisposable _uiIndicator;
 
-    private Action CanacleFlash;
-    private bool _leftCrackAnimationHappened;
-    private bool _rightCrackAnimationHappened;
-    private bool _BreakAnimationHappened;
+    private Action _canacleFlash;
+
+    public static Transform CoreTransform { get; private set; }
     
-    public event Action<IEntityTargetAbleComponent> OnTargetDisable;
+    public event Action<ITargetAbleEntity> OnTargetDisable;
+    
     public bool IsTargetAble => true;
 
     public PathCreator PatrolPath => _patrolPath;
 
     public EntityType EntityType => EntityType.Core;
-
-    public Stat InvincibleTime => throw new System.NotImplementedException();
-
-    public bool IsDamageable => true; //temp
-
-    public Stat Health => _hpStat;
-
-    public StatHandler StatHandler => throw new System.NotImplementedException();
-
-    public System.Action OnHealthChanged;
-
-    //SUPER TEMP! this needs to move to the Blackboard if we're really doing it
-    public static Transform CoreTrans;
-
-    public bool IsEntityDead { get; private set; }
+    public IEntityHealthComponent EntityHealthComponent { get; private set; }
+    public IEntityStatComponent EntityStatComponent { get; }//not in use
+    public IEntityVisualComponent EntityVisualComponent { get; }// not in use
     
-    public Dictionary<int, Stat> Stats { get; }
-    
-    public IEnumerable<IStatHolder> GetNestedStatHolders()
+    public bool IsInitialization { get; private set; }
+    public void Init()
     {
-        throw new System.NotImplementedException();
-    }
-    
-    protected override void Awake()
-    {
-        CoreTrans = transform;
-        _hpStat = new Stat("Health", _hp, int.MaxValue, 0); //TEMP! Requires a config
-
-        _uiIndicator = UIIndicatorHandler.SetNewIndicator(CoreTrans, new UIIndicatorConfig()
+        CoreTransform = transform;
+        
+        var config = new HealthComponentConfig()
         {
-            Image = _defaultCoreSpriteRenderer.sprite,
+            HealthComponentType = HealthComponentType.Standard,
+            HealthStat = _hp,
+        };
+        
+        EntityHealthComponent = HealthComponentFactory.GetHealthComponent(config);
+        AddComponent(EntityHealthComponent);
+        EntityHealthComponent.Init(this,config);
+        
+        _hpBar.Init(EntityHealthComponent.Health.BaseValue);
+        
+        EntityHealthComponent.Health.OnValueChanged += OnHealthChanage;
+        
+        _uiIndicator = UIIndicatorHandler.SetNewIndicator(transform, new UIIndicatorConfig()
+        {
+            Image = _spriteRenderer.sprite,
             Color = Color.white,
             AllwaysShow = false,
             DisposOnClick = false,
@@ -88,8 +78,12 @@ public class CoreTemple : BaseGameEntity, IEntityTargetAbleComponent
                 FlashingColor = Color.red
             }
         },null,GoToCore);
-        
-        base.Awake();
+
+    }
+
+    private void OnHealthChanage(StatChangeData statChangeData)
+    {
+        _hpBar.SetBarValue(statChangeData.NewValue);
     }
 
     private void GoToCore()
@@ -102,80 +96,25 @@ public class CoreTemple : BaseGameEntity, IEntityTargetAbleComponent
         base.Update();
 
         if (_enemies.Count > 0)
-            CanacleFlash = UIIndicatorHandler.StartFlashOnIndicator(_uiIndicator.ObjectInstanceId);
+            _canacleFlash = UIIndicatorHandler.StartFlashOnIndicator(_uiIndicator.ObjectInstanceId);
         else
-            CanacleFlash?.Invoke();
-    }
-
-    private void OnDisable() //override OnDestroy() instead?
-    {
-        CoreTrans = null;
-    }
-
-    public void Heal(float amount)
-    {
-        _hpStat.ProcessStatModifier(new StatModifier(amount,StatusModifierType.Addition),"Heal");
-        OnHealthChanged?.Invoke();
-    }
-
-    public void TakeDamage(float damage, bool isCrit)
-    {
-        if (_hpStat.CurrentValue <= 0 && !_BreakAnimationHappened)
-        {
-            _brokenCoreSpriteRenderer.SetActive(true);
-            _defaultCoreSpriteRenderer.enabled = false;
-            GameManager.CameraHandler.SetCameraPosition(transform.position);
-            GameManager.CameraHandler.LockCameraWithEase(transform.position,_animationCameraZoom);
-            _glowParticleSystem.gameObject.SetActive(true);
-            _glowParticleSystem.Play();
-            Invoke(nameof(DeathAnimation),_deathAnimationTime);
-            _BreakAnimationHappened = true;
-            return;
-        }
-        var hpRatio = _hpStat.CurrentValue / _hpStat.BaseValue;
-        if (hpRatio <= 0.6667 && !_rightCrackAnimationHappened)
-        {
-            _coreAnimator.SetBool("R_Crack",true);
-            _rightCrackAnimationHappened = true;
-        }
-        if (hpRatio <= 0.3336 && !_leftCrackAnimationHappened)
-        {
-            _coreAnimator.SetBool("L_Crack",true);
-            _leftCrackAnimationHappened = true;
-        }
-       
-        
-        _hpStat.ProcessStatModifier(new StatModifier(damage,StatusModifierType.Reduce),"Damage");
-        OnHealthChanged?.Invoke();
-
-    }
-
-    public void StartDeathSequence()
-    {
-        print("GAME OVER!");
-        IsEntityDead = true;
-    }
-
-    private void DeathAnimation()
-    {
-        _coreAnimator.SetBool("Break",true);
-        _glowParticleSystem.gameObject.SetActive(false);
-        Invoke(nameof(StartDeathSequence), _deathAnimationTime);
+            _canacleFlash?.Invoke();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent<Enemy>(out var enemy))
-        {
+        if (other.TryGetComponent<UnitEntity>(out var enemy))
             _enemies.Add(enemy);
-        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.TryGetComponent<Enemy>(out var enemy))
-        {
+        if (other.TryGetComponent<UnitEntity>(out var enemy))
             _enemies.Remove(enemy);
-        }
+    }
+
+    private void OnDestroy()
+    {
+        EntityHealthComponent.Health.OnValueChanged -= OnHealthChanage;
     }
 }

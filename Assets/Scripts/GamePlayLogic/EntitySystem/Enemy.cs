81dbd1dc74994edf2586b663d58e63a2dcd9d6c7
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2023-12-28 11:20:39 +0200

    core colliders fix

--- Assets/Scripts/GamePlayLogic/EntitySystem/Enemy.cs
+++ Assets/Scripts/GamePlayLogic/EntitySystem/Enemy.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Enemy : UnitEntity , IPoolable<Enemy>
 {
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Core"))
-        { 
-            EntityHealthComponent.StartDeathSequence();
-        }
-    }
+   
 
     public event Action<Enemy> OnDispose;
 }
